c661ce2929da1f84292167802b5d424f68ffde73
정동진[Android] <eastar.j@navercorp.com>
2023-03-15 20:30:43 +0900

    28장

--- EastarCustomLayout2/app/src/main/java/dev/eastar/customlayout/MainActivity.kt
+++ EastarCustomLayout2/app/src/main/java/dev/eastar/customlayout/MainActivity.kt
@@ -3,13 +3,18 @@ package dev.eastar.customlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.eastar.customlayout.ui.theme.EastarCustomLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
             EastarCustomLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -27,17 +32,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Box {
+        CascadeLayout(spacing = 20) {
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Blue)
+            )
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Red)
+            )
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Magenta)
+            )
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Cyan)
+            )
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Green)
+            )
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Gray)
+            )
+        }
+    }
 }
+
+
+@Composable
+fun CascadeLayout(
+    spacing: Int = 0,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        var indent = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            val placeables = measurables.map { measurable ->
+                measurable.measure(constraints)
+            }
+            var yCoord = 0
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = indent, y = yCoord)
+                indent += placeable.width + spacing
+                yCoord += placeable.height + spacing
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EastarCustomLayoutTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
