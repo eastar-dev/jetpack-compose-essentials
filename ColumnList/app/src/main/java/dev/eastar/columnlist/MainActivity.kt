327d86da07eb7b3e1eed05188f1bd0ad3c3830b2
정동진[Android] <eastar.j@navercorp.com>
2023-03-22 22:04:50 +0900

    34장 column base list scroll

--- ColumnList/app/src/main/java/dev/eastar/columnlist/MainActivity.kt
+++ ColumnList/app/src/main/java/dev/eastar/columnlist/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.eastar.columnlist.ui.theme.ColumnListTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,18 +43,40 @@ fun MainScreen() {
 
 @Composable
 fun ColumnListScreen() {
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+
     Column {
-        repeat(500) {
-            Text(
-                "List Item $it",
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(5.dp)
-            )
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollTo(0)
+                }
+            }) {
+                Text("Top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollTo(scrollState.maxValue)
+                }
+            }) {
+                Text("Bottom")
+            }
+        }
+
+        Column(Modifier.verticalScroll(scrollState)) {
+            repeat(500) {
+                Text(
+                    "List Item $it",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(5.dp)
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ColumnListTheme {
