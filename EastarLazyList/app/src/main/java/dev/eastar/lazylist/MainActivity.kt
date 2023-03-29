a729eec170a0e1b5221661fedd65a2d47751e6a3
정동진[Android] <eastar.j@navercorp.com>
2023-03-29 21:53:09 +0900

    36장 lazy list에 해더 달고 맨위로 버튼 달기

--- EastarLazyList/app/src/main/java/dev/eastar/lazylist/MainActivity.kt
+++ EastarLazyList/app/src/main/java/dev/eastar/lazylist/MainActivity.kt
@@ -1,11 +1,19 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package dev.eastar.lazylist
 
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,21 +22,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import dev.eastar.lazylist.ui.theme.EastarLazyListTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val items by lazy { resources.getStringArray(R.array.car_array) }
@@ -50,16 +64,70 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(items: Array<String>, modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    val groupedItems = items.groupBy { it.substringBefore(" ") }
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val displayButton = listState.firstVisibleItemIndex > 5
+
 
     val onListItemClick = { text: String ->
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
-    LazyColumn(modifier = modifier) {
-        items(items) {
-            MyListItem(item = it, onItemClick = onListItemClick)
+    Box {
+        LazyColumn(
+            modifier = modifier,
+            state = listState,
+            contentPadding = PaddingValues(40.dp),
+        ) {
+            groupedItems.forEach { (key, value) ->
+                stickyHeader() {
+                    Box(
+                        Modifier
+                            .background(Color.Magenta)
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            text = key, color = Color.White, modifier = Modifier
+                                .background(Color.DarkGray)
+                                .fillMaxWidth(), style = MaterialTheme.typography.titleMedium
+                        )
+                    }
+                }
+
+                items(value) {
+                    MyListItem(item = it, onItemClick = onListItemClick)
+                }
+            }
+        }
+
+        AnimatedVisibility(
+            visible = displayButton,
+            Modifier.align(Alignment.BottomCenter)
+        ) {
+
+            OutlinedButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.scrollToItem(0)
+                    }
+                },
+
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.DarkGray
+                ),
+                modifier = Modifier
+                    .background(Color.Magenta)
+                    .padding(5.dp)
+            ) {
+                Text(text = "Top")
+            }
+
         }
     }
+
     //MyListItem(item = items[0])
 }
 
