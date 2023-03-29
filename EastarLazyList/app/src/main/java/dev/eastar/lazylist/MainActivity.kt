1b30b1891acec4ae5dbde471a91cb370376ce6fb
정동진[Android] <eastar.j@navercorp.com>
2023-03-29 21:32:19 +0900

    event add

--- EastarLazyList/app/src/main/java/dev/eastar/lazylist/MainActivity.kt
+++ EastarLazyList/app/src/main/java/dev/eastar/lazylist/MainActivity.kt
@@ -1,9 +1,11 @@
 package dev.eastar.lazylist
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -21,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -45,20 +49,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(items: Array<String>, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    val onListItemClick = { text: String ->
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+
     LazyColumn(modifier = modifier) {
-        items(items.size) {
-            MyListItem(item = items[it])
+        items(items) {
+            MyListItem(item = it, onItemClick = onListItemClick)
         }
     }
     //MyListItem(item = items[0])
 }
 
 @Composable
-fun MyListItem(item: String) {
+fun MyListItem(item: String, onItemClick: (String) -> Unit) {
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable { onItemClick(item) },
         elevation = CardDefaults.cardElevation(),
         shape = RoundedCornerShape(10.dp)
     ) {
