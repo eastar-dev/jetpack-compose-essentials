7c8b45651f2f0e8c98b64a4ef7631b9b3d815454
정동진[Android] <eastar.j@navercorp.com>
2023-04-10 13:55:23 +0900

    41장 view model

---
+++ EastarViewModel/app/src/main/java/dev/eastar/viewmodel/MainViewModel.kt
@@ -0,0 +1,34 @@
+package dev.eastar.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlin.math.roundToInt
+
+class MainViewModel : ViewModel() {
+
+    var isFahrenheit by mutableStateOf(true)
+    var result by mutableStateOf("")
+
+
+    fun convertTemp(temp: String) {
+        result = kotlin.runCatching {
+            val tempInt = temp.toInt()
+
+            if (isFahrenheit) {
+                "${fahrenheitToCelsius(tempInt)}°C"
+            } else {
+                "${celsiusToFahrenheit(tempInt)}°F"
+            }
+        }.getOrDefault("Invalid Entry")
+    }
+
+    private fun celsiusToFahrenheit(tempInt: Int): String {
+        return ((tempInt * 1.8) + 32).roundToInt().toString()
+    }
+
+    private fun fahrenheitToCelsius(tempInt: Int): String {
+        return ((tempInt - 32) * 0.5556).roundToInt().toString()
+    }
+}
